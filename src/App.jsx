e2857c36f8187bb15e35169df43244b5b92a1f84
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2025-04-15 22:21:17 +0530

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-function List(props)
+ /* function List(props)
 {
 
   if(!props.animals)
@@ -20,16 +20,51 @@ function List(props)
 }
 
 
-function App() {
 
-  const animals = ["Lion", "Tiger", "Leopard", "LIzrd"]
+
+function TodoList() {
+
+  const todos = [
+    {task: "go to gym", id: crypto.randomUUID()},
+    {task: "go to bed", id: crypto.randomUUID()},
+    {task: "feed the cat", id: crypto.randomUUID()}
+  ];
+  // const animals = ["Lion", "Tiger", "Leopard", "LIzrd"]
 
   return (
+    // <div>
+    //   <h1>Animals</h1>
+    //   <List animals={animals} />
+    // </div>
     <div>
-      <h1>Animals</h1>
-      <List animals={animals} />
+        <ul>
+      {todos.map((todo) => {
+          <li key={todo.id}>{todo.task}</li>
+      })}
+    </ul>
     </div>
+    
+  )
+}
+*/
+
+function Button(props){
+  const buttonStyle = {
+    color: props.color,
+    fontSize: props.fontSize + 'px'
+  }
+
+  return (
+    <button style={buttonStyle}>{props.text}</button>
   )
 }
 
-export default App
+export default function App(){
+  return (
+    <div>
+      <Button text="Click me!"  color="blue" fontSize={12} />
+      <Button text="dont click me" color="red" fontSize={14}  />
+      <Button text="click me" color="green"  fontSize={20} />
+    </div>
+  )
+}
